2651c6acb309c32a51b3008a72f0026b244e6ac4
Chris-Abney-Carth <cabney@carthage.edu>
2025-05-06 14:06:56 -0500

    Developed Main Menu

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void printMainMenu(){
+    cout <<"Enter the number to select" << endl;
+    cout << "1) Take Attendance" << endl;
+    cout << "2) Display Attendance" << endl;
+    //cout << "3) Display Rate of Attendance on a Day" << endl;
+    cout << "3) Show Student Attendance Info" << endl;
+    cout << "4) Add Course" << endl;
+    cout << "5) Exit" << endl;
+}
+
 int main()
 {
-    cout<<"Agile Project"<<endl;
+    cout << "Welcome To The Attendance Assistant\n" << endl;
+    printMainMenu();
+    //cin a int to select
+    int menuSelection = 0;
+    string tempSelect = "";
+    cin >> tempSelect;
     return 0;
 }
