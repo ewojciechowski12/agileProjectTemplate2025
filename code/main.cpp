2dd5e67e88d08b32776ab50ab6104bad1f7e8dec
Chris-Abney-Carth <cabney@carthage.edu>
2025-05-08 13:01:13 -0500

    Added the menu selction
    added event handler and fucntion to detect if input is numbers

--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <ctype.h>
 using namespace std;
 
 void printMainMenu(){
@@ -10,14 +12,60 @@ void printMainMenu(){
     cout << "4) Add Course" << endl;
     cout << "5) Exit" << endl;
 }
-
+bool is_digits(string& str)
+{
+    bool toReturn = true;
+    for (char ch : str) {
+        int v = ch; // ASCII Val converted
+        if (!(ch >= 48 && ch <= 57)) {
+            //cout << "False" << endl;
+            toReturn = false;
+        }
+        
+    }
+ 
+    //cout << "True" << endl;
+    return toReturn;
+}
 int main()
 {
+    bool endProgram = false;
     cout << "Welcome To The Attendance Assistant\n" << endl;
     printMainMenu();
     //cin a int to select
     int menuSelection = 0;
     string tempSelect = "";
-    cin >> tempSelect;
+    
+    while (endProgram == false){
+        cin >> tempSelect;
+        while (is_digits(tempSelect) == false){
+        cout <<"\nPlease only enter in numbers" << endl;
+        printMainMenu();
+        cin >> tempSelect;
+        }
+        menuSelection = stoi(tempSelect);
+        switch (menuSelection){
+            case 1:
+                cout <<"\nTake Attendance" << endl;
+                break;
+            case 2:
+                cout <<"\nDisplay Attendance" << endl;
+                break;
+            case 3:
+                cout <<"\nShow Student Attendace" << endl;
+                break;
+            case 4:
+                cout <<"\nAdd Corse" << endl;
+                break;  
+            case 5:
+                endProgram = true;
+                break;
+            default:
+            cout <<"\nERR, Please only enter in the numbers to select an option" << endl;
+            break;  
+            
+        }
+        printMainMenu();
+    }
     return 0;
 }
